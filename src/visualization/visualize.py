ee6360923f080dbec96ed42f3ed2004ee1af0d41
James ID <idowu.james@rocketmail.com>
2024-01-20 20:18:10 +0100

    Plotting Multiple Axis

--- src/visualization/visualize.py
+++ src/visualization/visualize.py
@@ -62,3 +62,16 @@ fig, ax = plt.subplots()
 participant_df.groupby(["participant"])["acc_y"].plot()
 ax.set(xlabel="Samples", ylabel="acc_y")
 plt.legend()
+
+# --------------------------------------------------------------
+# Plotting Multiple Axis
+# --------------------------------------------------------------
+
+label='squat'
+participant = "A"
+all_axis_df = df.query(f"label=='{label}'").query(f"participant=='{participant}'").reset_index()
+
+fig, ax = plt.subplots()
+all_axis_df[['acc_y','acc_z','acc_x']].plot(ax=ax)
+ax.set(xlabel="Samples", ylabel="acc_y")
+plt.legend()
