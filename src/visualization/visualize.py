bdc6492d71dae86ac7ef6be34a031287a54efdc9
James ID <idowu.james@rocketmail.com>
2024-01-20 18:13:59 +0100

    initial plot analysis

---
+++ src/visualization/visualize.py
@@ -0,0 +1,40 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+from IPython.display import display
+
+# --------------------------------------------------------------
+# Load Data
+# --------------------------------------------------------------
+df = pd.read_pickle("../../data/interim/01_data_processed.pkl")
+
+# --------------------------------------------------------------
+# Plot Single Columns
+# --------------------------------------------------------------
+
+set_df = df[df["set"] == 2]
+plt.plot(set_df["acc_y"].reset_index(drop=True))
+
+# --------------------------------------------------------------
+# Plot All Exercises
+# --------------------------------------------------------------
+
+for label in df["label"].unique():
+    subset = df[df["label"] == label]
+    fig, ax = plt.subplots()
+    plt.plot(subset["acc_y"].reset_index(drop=True), label=label)
+    plt.legend()
+
+# --------------------------------------------------------------
+# Plot First 100 Exercises
+# --------------------------------------------------------------
+for label in df["label"].unique():
+    subset = df[df["label"] == label]
+    fig, ax = plt.subplots()
+    plt.plot(subset[:100]["acc_y"].reset_index(drop=True), label=label)
+    plt.legend()
+
+
+# --------------------------------------------------------------
+# Adjusting plot settings
+# --------------------------------------------------------------
